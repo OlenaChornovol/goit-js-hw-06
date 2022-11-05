bff863cfe46e2604861e3e5065a2e2c927879db7
Olena Chornovol <chornovol.elena@gmail.com>
2022-11-06 00:29:11 +0100

    task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+for (const title of ingredients) {
+  const newTagRef = document.createElement("li");
+  newTagRef.textContent = title;
+  newTagRef.classList.add("item");
+  ingredientsRef.append(newTagRef);
+}
